5261d746417a68048a310c74efff6a307778d2b4
David07994415 <david.game.power@gmail.com>
2024-04-06 22:03:30 +0800

    Feat:Add Directory_Recursion.cs

---
+++ WebApplication1/Views/Shared/Directory_Recursion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Views.Shared
+{
+    public static class Directory_Recursion
+    {
+        public static string GetUserName()
+        {
+            // 在这里获取您的数据，例如从数据库或服务中
+            var userName = "John Doe"; // 示例数据
+
+            return userName;
+        }
+    }
+}
