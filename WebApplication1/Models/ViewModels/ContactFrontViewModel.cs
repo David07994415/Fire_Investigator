a012f6b288e27316098eb8c81ae3bbef253df21e
David07994415 <david.game.power@gmail.com>
2024-04-07 21:57:12 +0800

    Feat:Add Contact Front View Model and Update View Page HTML

---
+++ WebApplication1/Models/ViewModels/ContactFrontViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class ContactFrontViewModel
+    {
+        [Required]
+        [MaxLength(60)]
+        public string name { get; set; }
+        [Required]
+        public sex gender { get; set; }
+        [Required]
+        public string phone { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string content { get; set; }
+        [Required]
+        public string validationCode { get; set; }
+    }
+    public enum sex
+    {
+        女 = 0,
+        男 = 1,
+    }
+}
