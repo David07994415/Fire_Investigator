2ee65c539851a0ccab1e8c1ee86959a8acaef84f
David07994415 <david.game.power@gmail.com>
2024-04-09 07:58:41 +0800

    Feat: Add Directory Model into DB

---
+++ WebApplication1/Migrations/202404082353593_Feature-Directroy-DBSheet.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FeatureDirectroyDBSheet : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Directory",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 100),
+                        Value = c.String(nullable: false, maxLength: 400),
+                        RecursiveId = c.Int(),
+                        CreateTime = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Directory", t => t.RecursiveId)
+                .Index(t => t.RecursiveId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Directory", "RecursiveId", "dbo.Directory");
+            DropIndex("dbo.Directory", new[] { "RecursiveId" });
+            DropTable("dbo.Directory");
+        }
+    }
+}
---
+++ WebApplication1/Models/Directory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [Table("Directory")]
+    public class Directory
+    {
+        [Key]
+        [Display(Name = "編號")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [MaxLength(100)]
+        [Display(Name = "目錄名稱")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [MaxLength(400)]
+        [Display(Name = "目錄代號")]
+        public string Value { get; set; }
+
+        [Display(Name = "目錄迴圈編號")]
+        public int? RecursiveId { get; set; } //可以沒有
+        [ForeignKey("RecursiveId")]
+        [Display(Name = "權限父成員")]
+        public virtual Directory ParentTable { get; set; }//virtual=虛擬資料，會跟資料庫的對應資料相對應
+
+
+        [Display(Name = "創立時間")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? CreateTime { get; set; }
+
+        [Display(Name = "權限子成員")]
+        public virtual ICollection<Directory> ChildTable { get; set; }
+    }
+}
