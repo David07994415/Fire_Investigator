d57f44bc4de537b5592ddb122fd048c86f094219
David07994415 <david.game.power@gmail.com>
2024-04-22 10:15:35 +0800

    Feat:Add Knowledge BackStage Index and Create Page

---
+++ WebApplication1/Models/ViewModels/CreateBackKnowledgeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class CreateBackKnowledgeViewModel
+    {
+        [Display(Name = "標題")]
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Display(Name = "是否顯示標籤")]
+        public bool? IsTop { get; set; }
+
+        [Display(Name = "是否顯示標籤")]
+        public bool? IsShow { get; set; }
+
+        [Display(Name = "發布資料之時間")]
+        public DateTime? IssueTime { get; set; }
+    }
+}
