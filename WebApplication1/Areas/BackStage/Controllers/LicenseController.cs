df5b90a146159d439ba1d1b17bf1b44f5f070248
David07994415 <david.game.power@gmail.com>
2024-04-24 16:59:51 +0800

    Feat:Add Business BackStage Controllers and Views

---
+++ WebApplication1/Areas/BackStage/Controllers/LicenseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Areas.BackStage.Filter;
+using WebApplication1.Models.ViewModels;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Areas.BackStage.Controllers
+{
+    [Authorize]
+    [AddBackLayoutComponent]
+    public class LicenseController : Controller
+    {
+        private DbModel db = new DbModel();
+        public ActionResult Index()
+        {
+            string ControllerName = RouteData.Values["Controller"].ToString();
+            var BusinessData = db.BusinessCategory.Where(x => x.Name == ControllerName).FirstOrDefault().BussinessTable.FirstOrDefault();
+            return View(BusinessData);
+        }
+        [HttpPost]
+        public ActionResult Index(EditBackBusinessViewModel input)
+        {
+            string UserName = User.Identity.Name;
+            var UserId = db.Member.FirstOrDefault(x => x.Account == UserName).Id;
+
+            string ckcontent = input.Content;
+            string ControllerName = RouteData.Values["Controller"].ToString();
+            var BusinessData = db.BusinessCategory.Where(x => x.Name == ControllerName).FirstOrDefault().BussinessTable.FirstOrDefault();
+            BusinessData.Content = ckcontent;
+            BusinessData.UpdateTime = DateTime.Now;
+            BusinessData.UpdateUser = UserId;
+
+            db.SaveChanges();
+            return View(BusinessData);
+        }
+    }
+}
---
+++ WebApplication1/Models/ViewModels/CreateBackBusinessViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class CreateBackBusinessViewModel
+    {
+    }
+    public class EditBackBusinessViewModel
+    {
+        [Required]
+        [AllowHtml]
+        public string Content { get; set; }
+    }
+}
