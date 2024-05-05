17a236c239692ae406da3d5f2c97286d612c9a20
David07994415 <david.game.power@gmail.com>
2024-05-05 23:01:02 +0800

    Feat:Add Member_Login Controller and Index View

---
+++ WebApplication1/Controllers/Member_LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Filter;
+
+namespace WebApplication1.Controllers
+{
+
+    [AddLayoutBreadcrumb("controller")]
+    [AddLayoutSidebar("controller")]
+    [AddLayoutMenu]
+
+    public class Member_LoginController : Controller
+    {
+        // GET: Member_Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
