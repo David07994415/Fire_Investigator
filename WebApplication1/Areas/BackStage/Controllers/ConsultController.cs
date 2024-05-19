abbe690643d76c771bb1044dba5ff0161866043e
David07994415 <david.game.power@gmail.com>
2024-05-19 11:52:55 +0800

    Update:Adjust BackStage Business Controllers and Views

--- WebApplication1/Areas/BackStage/Controllers/ConsultController.cs
+++ WebApplication1/Areas/BackStage/Controllers/ConsultController.cs
@@ -20,21 +20,35 @@ namespace WebApplication1.Areas.BackStage.Controllers
             var BusinessData = db.BusinessCategory.Where(x => x.Name == ControllerName).FirstOrDefault().BussinessTable.FirstOrDefault();
             return View(BusinessData);
         }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Index(EditBackBusinessViewModel input)
         {
-            string UserName = User.Identity.Name;
-            var UserId = db.Member.FirstOrDefault(x => x.Account == UserName).Id;
-
-            string ckcontent = input.Content;
             string ControllerName = RouteData.Values["Controller"].ToString();
             var BusinessData = db.BusinessCategory.Where(x => x.Name == ControllerName).FirstOrDefault().BussinessTable.FirstOrDefault();
-            BusinessData.Content = ckcontent;
-            BusinessData.UpdateTime = DateTime.Now;
-            BusinessData.UpdateUser = UserId;
 
-            db.SaveChanges();
-            return View(BusinessData);
+            if (ModelState.IsValid)
+            {
+                string UserName = User.Identity.Name;
+                var UserId = db.Member.FirstOrDefault(x => x.Account == UserName).Id;
+
+                string ckcontent = input.Content;
+                BusinessData.Content = ckcontent;
+                BusinessData.UpdateTime = DateTime.Now;
+                BusinessData.UpdateUser = UserId;
+
+                db.SaveChanges();
+
+                TempData["UpdateCompleted"] = true;
+
+                return View(BusinessData);
+            }
+            else 
+            {
+                ModelState.AddModelError("Content", "CK Editor必填相關內容");
+                return View(BusinessData);
+            }
         }
     }
 }
